55a9c499baeaa285f0940f5601f2fb20b5314073
mahmoud10 <mr7721897@gmail.com>
2023-05-14 17:05:56 +0300

    last

--- script.js
+++ script.js
@@ -7,13 +7,18 @@ var swiper = new Swiper('.swiper', {
     slidesPerView: 3,
     spaceBetween: 30,
     breakpoints: {
-        280: {
+        0: {
             slidesPerView: 1,
-            spaceBetweenSlides: 50
+            spaceBetweenSlides: 30
+        },
+
+        768: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 20
         },
         999: {
-            slidesPerView: 3,
-            spaceBetweenSlides: 50
+            slidesPerView: 6,
+            spaceBetweenSlides: 1
         }
     }
 });
@@ -40,3 +45,50 @@ let stop =setInterval(function(){
 },3/count)
 
 })
+// auto write funcation 
+var header = document.querySelector(".auto-write");
+const text = "AL FATOON";
+let idx = 1;
+
+function autoWrite(){
+    header.innerHTML = text.slice(0,idx);
+
+    idx++
+
+    if(idx > text.length){
+        idx = 1 ;
+
+    }
+
+    setTimeout(autoWrite, 300)
+
+}
+autoWrite();
+//*********************** */
+var links = document.querySelectorAll(" .header-nav nav  ul li  a ");
+links.forEach(a=>{
+    a.addEventListener("click",function(){
+        resetlinks();
+        a.classList.add("active")
+    })
+})
+function resetlinks (){
+    links.forEach(a=>{
+        a.classList.remove("active")
+    })
+}
+// scroll btn /////           
+let mybutton = document.getElementById("myBtn")  
+
+window.onscroll = function (){
+    if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
+        mybutton.style.display="block"
+    }else{
+        mybutton.style.display= "none";
+    }
+}
+
+function topFunction (){
+    document.body.scrollTop=0;
+    document.documentElement.scrollTop=0;
+}
